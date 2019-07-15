18a73aaf52a8b524817558dd6a1237193f873759
hp <hp@DESKTOP-HH0CB76>
2019-07-15 11:12:04 +0800

    SpringBoot进行web开发简单示例

---
+++ SpringBoot-Web/src/main/java/com/hp/SpringBootWebApplication.java
@@ -0,0 +1,14 @@
+package com.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @SpringBootApplication为SpringBoot启动类的注解
+ */
+@SpringBootApplication
+public class SpringBootWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
