502d0532b640ec65366969be1d7d8a830592e651
hp <hp@DESKTOP-HH0CB76>
2019-08-21 16:05:01 +0800

    新建构建RESTful API与单元测试

--- SpringBoot-Web/src/main/java/com/hp/controller/HelloController.java
+++ SpringBoot-Web/src/main/java/com/hp/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/hello")
+	@RequestMapping(value="/hello")
 	public String index(ModelMap map) {
 		// 加入一个属性，用来在模板中读取
 		map.addAttribute("host", "http://blog.didispace.com");
