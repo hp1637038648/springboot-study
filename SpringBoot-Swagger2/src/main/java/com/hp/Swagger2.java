d081ceacfc81dc446097b1ad05dfeb8854ba5403
hp <hp@DESKTOP-HH0CB76>
2019-08-27 16:09:55 +0800

    springboot使用Swagger2的项目

---
+++ SpringBoot-Swagger2/src/main/java/com/hp/Swagger2.java
@@ -0,0 +1,44 @@
+package com.hp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+/**
+ * 通过@Configuration注解，让Spring来加载该类配置
+ * 在通过@EnableSwagger2注解来启用Swagger2，表示这是一个Spring Swagger的配置文件。
+ * @author hp
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+	
+	@Bean
+	public Docket createRestApi() {
+		//DocumentationType.SWAGGER_2作为Docket构造方法的参数，指定了所用的swagger版本2.0
+		return new Docket(DocumentationType.SWAGGER_2) 
+				// apiInfo则是调用接下来的apiInfo函数，来创建Docket的信息
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.hp.controller"))  //api接口包扫描路径
+				.paths(PathSelectors.any()) //可以根据url路径设置哪些请求加入文档，忽略哪些请求
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		// apiInfo函数采用ApiInfoBuilder来创建ApiInfo类
+		return new ApiInfoBuilder()
+				.title("Spring Boot中使用Swagger2构建RESTful APIs")
+				.description("Spring Boot入门学习相关代码地址：https://github.com/hp1637038648/springboot-study")
+				.termsOfServiceUrl("https://github.com/hp1637038648/springboot-study")  //设置文档的License信息
+				.contact("胡蓬")
+				.version("1.0")
+				.build();
+	}
+}
--- SpringBoot-Swagger2/src/main/java/com/hp/controller/UserController.java
+++ SpringBoot-Swagger2/src/main/java/com/hp/controller/UserController.java
@@ -14,17 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hp.entity.User;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 /*
  * @author hp
  * @date 2019/8/22
  */
+@Api(value = "用户相关接口", description = "提供用户相关的 Rest API")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
 	// 创建线程安全的Map
 	public static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
-
+	
+	@ApiOperation(value="获取用户列表", notes="获取所有用户信息")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public List<User> getUserList() {
 		// 处理"/users/"的GET请求，用来获取用户列表
@@ -32,7 +39,9 @@ public class UserController {
 		List<User> r = new ArrayList<User>(users.values());
 		return r;
 	}
-
+    
+	@ApiOperation(value = "创建用户",notes="根据User对象创建用户")
+	@ApiImplicitParam(name="user",value = "用户详细实体user",required = true, dataType = "User")
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String postUser(@ModelAttribute User user) {
 		// 处理"/users/"的POST请求，用来创建User
@@ -40,14 +49,21 @@ public class UserController {
 		users.put(user.getId(), user);
 		return "success";
 	}
-
+    
+	@ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+	@ApiImplicitParam(name = "id",value = "用户ID",required = true,dataType = "Long",paramType="path")
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public User getUser(@PathVariable Long id) {
 		// 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
 		// url中的id可通过@PathVariable绑定到函数的参数中
 		return users.get(id);
 	}
-
+    
+	@ApiOperation(value = "更新用户详细信息",notes = "根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id",value = "用户",required = true, dataType = "Long",paramType = "path"),
+		@ApiImplicitParam(name = "user",value = "用户详细实体user",required = true, dataType = "User")
+	})
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public String putUser(@PathVariable Long id, @ModelAttribute User user) {
 		// 处理"/users/{id}"的PUT请求，用来更新User信息
@@ -58,6 +74,8 @@ public class UserController {
 		return "success";
 	}
 
+	@ApiOperation(value = "删除用户",notes = "根据url的ID来删除指定对象")
+	@ApiImplicitParam(name="id",value = "用户ID",required = true,dataType = "Long",paramType = "path")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public String deleteUser(@PathVariable Long id) {
 		// 处理"/users/{id}"的DELETE请求，用来删除User
