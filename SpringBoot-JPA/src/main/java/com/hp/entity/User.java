51b407268a049cd4632b49f7deb117461148afb7
hp <hp@DESKTOP-HH0CB76>
2019-09-09 14:56:39 +0800

    创建了springboot整合JPA的入门项目

---
+++ SpringBoot-JPA/src/main/java/com/hp/entity/User.java
@@ -0,0 +1,57 @@
+package com.hp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_user") // 声明此对象映射到数据库的数据表，通过它可以为实体指定表(table)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Integer age;
+
+	public User() {
+
+	}
+
+	public User(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
