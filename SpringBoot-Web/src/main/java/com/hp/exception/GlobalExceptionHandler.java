0854dae9717ca9920d59781a1eba0830e32fc8ee
hp <hp@DESKTOP-HH0CB76>
2019-10-08 14:11:47 +0800

    更改了pom文件

--- SpringBoot-Web/src/main/java/com/hp/exception/GlobalExceptionHandler.java
+++ SpringBoot-Web/src/main/java/com/hp/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hp.exception.entity.ErrorInfo;
